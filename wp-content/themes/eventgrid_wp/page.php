0d58e7d697e05cf277954c4001121871717ebef4
Rogatnev Nikita <mail@rogatnev.ru>
2016-08-01 16:31:44 +0300

    Live Composer init

--- wp-content/themes/eventgrid_wp/page.php
+++ wp-content/themes/eventgrid_wp/page.php
@@ -4,8 +4,6 @@
 		<!-- section -->
 		<section>
 
-			<h1><?php the_title(); ?></h1>
-
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 			<!-- article -->
@@ -39,7 +37,5 @@
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
